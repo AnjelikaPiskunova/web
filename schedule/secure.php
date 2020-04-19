ebe156d18d1104bf238c35c2691f8da5ddeb0ab7
AnjelikaPiskunova <63058078+AnjelikaPiskunova@users.noreply.github.com>
2020-04-19 11:48:17 +0600

    web
    web

--- schedule/secure.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    require_once 'autoload.php';
-    if (!isset($_SESSION['role']) || isset($_POST['out'])) {
-        session_destroy();
-        header('Location: auth.php');
-        exit;
-    }
